1d86d46085be0fdd26febe5356128ff2f0c19b3e
Nicolas O <nico.olivaresg@hotmail.cl>
2016-09-22 23:54:22 -0300

    Entrega final
    2 algoritmos implementados Combinatoria sin repetición

--- bingo.c
+++ bingo.c
@@ -4,9 +4,14 @@
 
 
 void imprimirCombinacionEnArchivo(FILE * file, int * a){
-	fprintf(file,"%d-%d-%d-%d-%d-%d-%d-%d\n", a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7]);
+	fprintf(file,"[%d-%d-%d-%d-%d-%d-%d-%d]\n", a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7]);
 }
 
+//Procedimiento encargado de obtener todas las combinaciónes
+//de números entre 1 a 20, sin repeticiones
+//Entrada: un archivo para imprimir las combinaciones
+//		   un numero entero para el limite 
+//Salida: entero para comprobación de estado
 int bingo_combinacion_2(FILE * file,int n){
 	int a,b,c,d,e,f,g,h,z,i;
 	if(n>=8)
@@ -54,64 +59,55 @@ int bingo_combinacion_2(FILE * file,int n){
 	}
 }
 
-int bingo_combinacion_1(FILE * file,int n){
-	int a,b,c,d,e,f,g,h;
-	if(n>=8)
-	{
-		for(a=1;a<=n-7;a++)
-		{
-			for(b=a+1;b<=n-6;b++)
-			{
-				for(c=b+1;c<=n-5;c++)
-				{
-					for(d=c+1;d<=n-4;d++)
-					{
-						for(e=d+1;e<=n-3;e++)
-						{
-							for(f=e+1;f<=n-2;f++)
-							{
-								for(g=f+1;g<=n-1;g++)
-								{
-									for(h=g+1;h<=n;h++)
-									{		
-										fprintf(file,"%d-%d-%d-%d-%d-%d-%d-%d\n", a,b,c,d,e,f,g,h);
-									}
-								}	
-							}	
-						}	
-					}	
-				}		
-			}	
-		}	
-		return 0;
-	}else{
-		return -1;
-	}
-}
 
-int bingo_combinacion(FILE * file,int n){
-	int z,i,j;
-	int * a = (int*)malloc(n*sizeof(int));
+int bingo_combinacion_1(FILE * file,int n){
+	int limite,i;
 	if(n>=8)
 	{
-		for(i=0;i<8;i++){
-			a[i] = i+1;
+		int * arr = (int*)malloc(8*sizeof(int));
+		for (i = 0; i < 8; i++)
+		{
+			arr[i]=i+1;
 		}
 
-		
-		imprimirCombinacionEnArchivo(file,a);
-					
+		fprintf(file,"Conjunto:\n[");
+		for(i=1;i<=n;i++){
+			if(i==n){
+				fprintf(file,"%d]", i);
+			}else{
+				fprintf(file,"%d-", i);
+			}
+		}
+		fprintf(file, "\nSubconjuntos:\n");
+		imprimirCombinacionEnArchivo(file,arr);
+		for(limite = n;limite>=n-7; limite--)
+		{
+			i=7;
+			while(i>=0)
+			{
+				while(arr[i]<limite){
+					arr[i]++;
+					imprimirCombinacionEnArchivo(file,arr);
+					if(arr[i]==limite){
+						limite--;
+						i--;
+					}
+				}
+				i--;
+			}	
+		}
 		return 0;
 	}else{
 		return -1;
 	}
 }
+
 
 int main(int argc, char* argv[]) {
   	int n;
 	int* conjunto;
 	int i;
-	FILE * fileIN,*fileOUT;
+	FILE * fileIN,*fileOUT, *fileOUT2;
 	switch (argc){
 		case 0:
 			printf("%s\n", "ARGUMENTOS INSUFICIENTES");
@@ -125,6 +121,7 @@ int main(int argc, char* argv[]) {
 			//printf("%s\n", argv[1]);
 			fileIN = fopen(argv[1],"r");
 			fileOUT = fopen("SALIDA.txt","w");
+			fileOUT2 = fopen("SALIDA2.txt","w");
 			if(fileIN){
 				if(fscanf(fileIN,"%d\n",&n)==EOF){
 					printf("%s\n", "ERROR DE LECTURA DE ARCHIVO");
@@ -133,8 +130,14 @@ int main(int argc, char* argv[]) {
 				{
 					conjunto = (int*)malloc(n*sizeof(int));	
 				}
-
-				bingo_combinacion_2(fileOUT,n);
+				if(bingo_combinacion_1(fileOUT,n)==0 && bingo_combinacion_2(fileOUT2,n)==0){
+					printf("%s\n", "COMBINACIONES EXITOSAS ALGORITMO 2, revisar archivo SALIDA2.txt");
+					printf("%s\n", "COMBINACIONES EXITOSAS ALGORITMO 1, revisar archivo SALIDA.txt");
+					exit(EXIT_SUCCESS);
+				}else{
+					printf("%s\n", "ERROR EN OBTENCION DE COMBINACIONES");
+					exit(EXIT_FAILURE);	
+				}
 				fclose(fileIN);	
 				fclose(fileOUT);
 				exit(EXIT_SUCCESS);
